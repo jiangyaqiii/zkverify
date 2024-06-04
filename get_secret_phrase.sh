d6a72a06e41be01d26035d4b3c5e87638eefd42b
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-05 00:29:00 +0800

    Update get_secret_phrase.sh

--- get_secret_phrase.sh
+++ get_secret_phrase.sh
@@ -1,1 +1,1 @@
-cat /home/test/compose-zkverify-simplified/deployments/validator-node/testnet/configs/node/secrets/secret_phrase.dat
+cat /home/zkverify/compose-zkverify-simplified/deployments/validator-node/testnet/configs/node/secrets/secret_phrase.dat
