2524d1f61fdb57ed409a4fdb86d80ff2953e2986
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-08 02:24:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,6 +15,7 @@ fi
 
 #安装所需软件
 sudo apt update
+sudo apt install -y git
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-commoncurl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
