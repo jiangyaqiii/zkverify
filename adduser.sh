e0f1f882449cc614b4f8bbd18c1509e7a209ccfd
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-08 03:01:54 +0800

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -6,9 +6,15 @@ if [ "$(id -u)" != "0" ]; then
     echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
     exit 1
 fi
-
-username=zkverify
-password=Aa112211.
+echo ''
+echo '由于zkverify节点只能在普通用户环境下按照，所以需要设置普通用户信息'
+echo ''
+read -p "请输入你的普通用户名: " username
+read -p "请输入你的普通用户名登录密码: " password
+echo ''
+echo '普通用户设置完毕，请退出'
+# username=zkverify
+# password=Aa112211.
 # 创建新用户
 sudo adduser $username --gecos "" --disabled-password
 
