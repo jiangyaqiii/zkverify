3a5adf53990fa6fbe5057223614f6d14bee6a73e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-13 23:09:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,21 +20,21 @@ cd compose-zkverify-simplified
 #启动rpc需要选择参数
 echo ''
 echo '启动rpc'
-echo -e "1\n1\n2\n2\n1" | scripts/init.sh
+scripts/init.sh
 docker compose -f /home/zkverify/compose-zkverify-simplified/deployments/rpc-node/testnet/docker-compose.yml up -d
 docker container ls
 
 #启动boot节点需要选择参数
 echo ''
 echo '启动boot节点'
-echo -e "3\n1\n2\n2" | scripts/init.sh
+scripts/init.sh
 docker compose -f /home/zkverify/compose-zkverify-simplified/deployments/boot-node/testnet/docker-compose.yml up -d
 docker container ls
 
 #启动validator节点需要选择参数
 echo ''
 echo '启动validator节点'
-echo -e "2\n1\n2\n2\2" | scripts/init.sh
+scripts/init.sh
 docker compose -f /home/zkverify/compose-zkverify-simplified/deployments/validator-node/testnet/docker-compose.yml up -d
 docker container ls
 cd
